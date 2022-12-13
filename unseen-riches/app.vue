6eb92564a5b6b88a01a4fe68a3a07fb8aeafaa94
unknown <jessehorner11@gmail.com>
2022-12-12 20:14:49 -0700

    linked up and good to go, time to code

--- unseen-riches/app.vue
+++ unseen-riches/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
+  <div class="flex flex-col min-h-screen bg-slate-400">
+    <!-- pages -->
+    <NuxtPage class="grow" />
   </div>
 </template>
---
+++ unseen-riches/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>It's alive</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
