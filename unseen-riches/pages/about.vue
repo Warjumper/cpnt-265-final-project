9d5375f312651fd8cf832bab2802def05130beb2
unknown <jessehorner11@gmail.com>
2022-12-12 20:14:49 -0700

    linked up and good to go, time to code

---
+++ unseen-riches/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="">About</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ unseen-riches/pages/events.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Let's build</h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
